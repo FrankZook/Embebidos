2ac95dbc683fe8be81b411dda2d26fd77d41b325
Francisco <franciscojavier_perez@ucol.mx>
2019-06-20 17:54:01 -0500

    Agragando practica al repositorio

---
+++ Contadordeanillo.c
@@ -0,0 +1,20 @@
+#include <18F4620.h>
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
+#use delay(clock=16000000)
+
+int16 contador=1;
+
+void main (void){
+   set_tris_b(0x00);
+while(1){
+   if(contador==128){
+      contador==1
+   }
+   output_b(contador);
+   contador*=2;
+   delay_ms(300);
+}
+
+
+}
