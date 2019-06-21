8b3ae577d68f79e99dceffd91057004920bfdc3b
Francisco <franciscojavier_perez@ucol.mx>
2019-06-20 19:06:23 -0500

    Agregando mascaras

--- main.c
+++ main.c
@@ -3,16 +3,28 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
-int16 contador=1;
+int contador_b=1;
+int contador_c=128;
+int cintador_d=1;
+int flagopcion1=0, flagopcion2=0, flagopcion3=0;
+int puertoA;
 
 void main (void){
+   set_tris_a(0x3F);
    set_tris_b(0x00);
+   set_tris_c(0x00);
+   set_tris_d(0x00);
 while(1){
-   output_b(contador);
-   contador*=2;
+   puertoA=input_a();
+   
+   if(contador_b==128){
+   contador_b==1;
+   }
+   if((puertoA&0x01)==0x01){
+   flagopcion1=1;
+   output_b(contador_b);
+   contador_b*=2;
    delay_ms(300);
-   if(contador==128){
-   contador==1;
    }
 }  
 
